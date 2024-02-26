3028f83506217c5fb9c6258daea187d84d5b7f1d
kptholia <rishab14bd@gmail.com>
2024-02-26 22:41:19 +0530

    second commit

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -19,6 +19,6 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
-    use SendsPasswordResetEmails;
-    use SendsPasswordResetEmails;
+    // use SendsPasswordResetEmails;
+    // use SendsPasswordResetEmails;
 }
